ae75c2d42e6a7052136ba1d3ce9f9921c5b7f50f
shatminkhan0 <sharminakterkhan0@gmail.com>
2023-07-30 03:53:32 +0600

    Modify app.js file

---
+++ app.js
@@ -0,0 +1,12 @@
+// Header Scroll
+let nav = document.querySelector(".navbar");
+window.onscroll = function () {
+    if(document.documentElement.scrollTop > 20){
+        nav.classList.add("header-scrolled");
+    }else{
+        nav.classList.remove("header-scrolled"); 
+    }
+}
+
+
+
