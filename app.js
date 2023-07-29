12580fb061c6b56ca48824e5e2e60cda9544cbc7
shatminkhan0 <sharminakterkhan0@gmail.com>
2023-07-30 03:54:25 +0600

    Modify app.js file

--- app.js
+++ app.js
@@ -9,4 +9,13 @@ window.onscroll = function () {
 }
 
 
+// nav hide
+let navbar = document.querySelectorAll(".nav-link");
+let navCollapse = document.querySelector(".navbar-collapse.collapse");
+navbar.forEach(function(a){
+    a.addEventListener("click", function(){
+        navCollapse.classList.remove("show");
+    })
+})
+
 
